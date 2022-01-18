04df340c23db1ac7ea0afe3df886be7c0088c8f0
Fabio Vaz <fabiovaz@gmail.com>
2022-01-18 12:21:46 -0300

    fix: google pdf

--- pages/cardapio/index.vue
+++ pages/cardapio/index.vue
@@ -1,29 +1,15 @@
 <template>
- <main>
-  <object data="./cardapio_primavera.pdf" type="application/pdf">
-    <iframe src="https://docs.google.com/viewer?url=https://xamam-rc.com.br/cardapio_primavera.pdf&embedded=true"></iframe>
-  </object>
- </main>
+  <main>
+    <iframe src="https://docs.google.com/viewerng/viewer?url=https://xamam-cardapio.vercel.app/cardapio_primavera.pdf&embedded=true" />
+  </main>
 </template>
 <script>
 export default {
-  mounted() {
-    // window.location.href='./cardapio_primavera.pdf'
-    // var a = document.createElement('A')
-    // var filePath = './cardapio_primavera.pdf'
-    // a.href = filePath
-    // a.download = filePath.substr(filePath.lastIndexOf('/') + 1)
-    // document.body.appendChild(a)
-    // a.click()
-    // document.body.removeChild(a)
-  }
 }
 </script>
-
 <style>
-object {
+iframe {
   width: 100vw;
   height: 100vh;
 }
 </style>
-
